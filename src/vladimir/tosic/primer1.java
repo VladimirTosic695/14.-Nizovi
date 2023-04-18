3063b00c10d3cdd88a686fc00331c875b1343687
VladimirTosic695 <vladimir.tosic695@gmail.com>
2023-04-19 00:25:00 +0200

    Osnove nizova.

---
+++ src/vladimir/tosic/primer1.java
@@ -0,0 +1,25 @@
+package vladimir.tosic;
+
+import java.util.Scanner;
+
+public class primer1 {
+
+	public static void main(String[] args) {
+		Scanner ulaz = new Scanner(System.in);
+		System.out.println("Unesite broj članova niza n: ");
+		int n = ulaz.nextInt(); 
+		// Rezervisanje prostora za niz od n članova
+		int x[] = new int[n];
+		System.out.println("Unesite članove niza:"); 
+		for (int i = 0; i < x.length; i++) { 
+			System.out.print("x[" + i + "] = ");
+			x[i] = ulaz.nextInt(); 
+		} 
+		//  Štampanje članove niza
+		for (int i = 0; i < x.length; i++)
+		 System.out.println(x[i]);
+		ulaz.close();
+
+	}
+
+}
